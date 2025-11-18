e8db4eb18396e53bec7f3a3e1244eac2459b9f8f
LAPTOP-M7T10733\jub12 <jub1234jub@gmail.com>
2025-11-18 17:07:23 +0200

    Working proof of concept

--- DoodleBosses/ModClass.cs
+++ DoodleBosses/ModClass.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UObject = UnityEngine.Object;
 
@@ -11,26 +12,72 @@ namespace DoodleBosses
     {
         internal static DoodleBosses Instance;
 
-        //public override List<ValueTuple<string, string>> GetPreloadNames()
-        //{
-        //    return new List<ValueTuple<string, string>>
-        //    {
-        //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
-        //    };
-        //}
+        public static readonly Dictionary<string, GameObject> _gameObjects = new();
 
-        //public DoodleBosses() : base("DoodleBosses")
-        //{
-        //    Instance = this;
-        //}
+        private Dictionary<string, ValueTuple<string, string>> _preloads = new()
+        {
+
+            [BossStrings[Bosses.SOUL_WARRIOR]] = ("GG_Mage_Knight", "Mage Knight"),
+            [BossStrings[Bosses.MANTIS_LORDS]] = ("Fungus2_15_boss", "Mantis Battle/Battle Main/Mantis Lord"),
+            [BossStrings[Bosses.GRUZ_MOTHER]] = ("GG_Gruz_Mother", "_Enemies/Giant Fly"),
+            
+        };
+
+        public Textures SpriteDict { get; private set; }
+
+        public static Sprite GetSprite(Bosses boss) => Instance.SpriteDict.Get(boss);
+
+        public override string GetVersion() => "0.0.1-0";
+
+        public override List<ValueTuple<string, string>> GetPreloadNames()
+        {
+            return _preloads.Values.ToList();
+        }
+
+        public DoodleBosses() : base("Doodle Boss project")
+        {
+            Instance = this;
+
+            SpriteDict = new Textures();
+        }
 
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
             Log("Initializing");
 
+            foreach (var (name, (scene, path)) in _preloads)
+            {
+                _gameObjects[name] = preloadedObjects[scene][path];
+            }
+
             Instance = this;
+
+            foreach (Bosses boss in BossStrings.Keys)
+            {
+                _gameObjects[BossStrings[boss]].GetComponent<tk2dSprite>().GetCurrentSpriteDef().material.mainTexture = GetSprite(boss).texture;
+
+            }
+            //_gameObjects[BossStrings[Bosses.GRUZ_MOTHER]].GetComponent<tk2dSprite>().GetCurrentSpriteDef().material.mainTexture = GetSprite(Bosses.GRUZ_MOTHER).texture;
 
             Log("Initialized");
         }
+
+        public enum Bosses
+        {
+            GRUZ_MOTHER,
+            
+            MANTIS_LORDS,
+
+            SOUL_WARRIOR
+        }
+
+        private static readonly Dictionary<Bosses, string> BossStrings = new Dictionary<Bosses, string>()
+        {
+            { Bosses.GRUZ_MOTHER, "GruzMother" },
+            { Bosses.MANTIS_LORDS, "MantisLord" },
+            { Bosses.SOUL_WARRIOR, "SoulWarrior" },
+        };
+
+
     }
 }
---
+++ DoodleBosses/Textures.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DoodleBosses
+{
+    public class Textures
+    {
+
+        private readonly Dictionary<DoodleBosses.Bosses, Sprite> _dict;
+
+        public Textures()
+        {
+
+            Assembly asm = Assembly.GetExecutingAssembly();
+            _dict = new Dictionary<DoodleBosses.Bosses, Sprite>();
+            var tmpTextures = new Dictionary<DoodleBosses.Bosses, string>
+            {
+                { DoodleBosses.Bosses.GRUZ_MOTHER, "DoodleBosses.Resources.Sheet_GruzMother.png" },
+                { DoodleBosses.Bosses.MANTIS_LORDS, "DoodleBosses.Resources.Sheet_MantisLord.png" },
+                { DoodleBosses.Bosses.SOUL_WARRIOR, "DoodleBosses.Resources.Sheet_SoulWarrior.png" },
+            };
+
+            foreach (var pair in tmpTextures)
+            {
+                using (Stream s = asm.GetManifestResourceStream(pair.Value))
+                {
+                    if (s != null)
+                    {
+                        byte[] buffer = new byte[s.Length];
+                        s.Read(buffer, 0, buffer.Length);
+                        s.Dispose();
+
+                        //Create texture from bytes
+                        var tex = new Texture2D(2, 2);
+
+                        tex.LoadImage(buffer, true);
+
+                        // Create sprite from texture
+                        _dict.Add(pair.Key, Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f)));
+                    }
+                }
+            }
+        }
+
+        public Sprite Get(DoodleBosses.Bosses key)
+        {
+            return _dict[key];
+        }
+    }
+}
