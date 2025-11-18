72ad73c5cb8f6a20e7db7b1320a1b1900dc1d222
LAPTOP-M7T10733\jub12 <jub1234jub@gmail.com>
2025-11-18 16:02:55 +0200

    Init

---
+++ DoodleBosses/ModClass.cs
@@ -0,0 +1,36 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace DoodleBosses
+{
+    public class DoodleBosses : Mod
+    {
+        internal static DoodleBosses Instance;
+
+        //public override List<ValueTuple<string, string>> GetPreloadNames()
+        //{
+        //    return new List<ValueTuple<string, string>>
+        //    {
+        //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
+        //    };
+        //}
+
+        //public DoodleBosses() : base("DoodleBosses")
+        //{
+        //    Instance = this;
+        //}
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Log("Initializing");
+
+            Instance = this;
+
+            Log("Initialized");
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# DoodleBosses
+
+A mod for the game Hollow Knight.
